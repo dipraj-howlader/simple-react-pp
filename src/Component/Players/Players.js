d4906d0103357718080606afbdf2a4efdf5e5bb1
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-03-01 01:33:27 +0600

    added

---
+++ src/Component/Players/Players.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from "react-bootstrap/Button";
+import '../../Component/assets/css/bootstrap.min.css';
+const Players = () => {
+    return (
+        <div>
+            <Button variant="warning">Clicked</Button>{' '}
+        </div>
+    );
+};
+
+export default Players;
